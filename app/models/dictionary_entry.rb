6b9982c4f6efb58878eddf3440343e7e8a1c27e1
Sam Russell <sam.russell@powershop.co.nz>
2015-10-29 12:02:02 +1300

    first tests!

--- app/models/dictionary_entry.rb
+++ app/models/dictionary_entry.rb
@@ -1,4 +1,9 @@
 class DictionaryEntry < ActiveRecord::Base
   has_many :boards
   has_many :submitted_words
+
+  validates :word, format: {
+    with: /\A[A-Z]+\z/,
+      message: "must be an uppercase string"
+  }
 end
--- spec/models/dictionary_entry_spec.rb
+++ spec/models/dictionary_entry_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe DictionaryEntry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'accepts uppercase words of non-zero length' do
+    expect(DictionaryEntry.create(word: 'ABCD')).to be_valid
+  end
+
+  it 'does not accept lowercase words' do
+    expect(DictionaryEntry.create(word: 'abcd')).to_not be_valid
+  end
+
+  it 'does not accept mixed case words' do
+    expect(DictionaryEntry.create(word: 'Abcd')).to_not be_valid
+  end
+
+  it 'does not accept zero length or nil words' do
+    expect(DictionaryEntry.create(word: '')).to_not be_valid
+    expect(DictionaryEntry.create(word: nil)).to_not be_valid
+  end
+
+  it 'does not accept non-alpha words words' do
+    expect(DictionaryEntry.create(word: '234')).to_not be_valid
+    expect(DictionaryEntry.create(word: '%!')).to_not be_valid
+  end
 end
