dd1bb061d80ad5cab3b38c2e2fcc8856cde91325
Sam Russell <sam.russell@powershop.co.nz>
2015-10-29 11:54:40 +1300

    relations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,4 @@
 class Board < ActiveRecord::Base
+  belongs_to :dictionary_entry
+  has_many :submitted_word
 end
--- app/models/submitted_word.rb
+++ app/models/submitted_word.rb
@@ -1,2 +1,4 @@
 class SubmittedWord < ActiveRecord::Base
+  belongs_to :board
+  belongs_to :dictionary_entry
 end
