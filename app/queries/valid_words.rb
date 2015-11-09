f5573163ab5b16bdf09ac4a65e54e79ffdfafdaf
Sam Russell <sam.russell@powershop.co.nz>
2015-11-09 16:03:48 +1300

    validwords quicker (also correct now)

--- app/queries/valid_words.rb
+++ app/queries/valid_words.rb
@@ -4,23 +4,20 @@ class ValidWords
   end
 
   def call
-    DictionaryWord.where('sorted_letters ~ :regex', regex: grid_regex).select &method(:is_word_valid?)
+    DictionaryWord.where('sorted_letters ~ :regex', regex: "^#{grid_regex}$")
   end
 
   private
 
   def grid_regex
-    @board.dictionary_word.word.chars.sort.each_with_object([]) do |char, array|
+    @board.dictionary_word.word.chars.each_with_object([]) do |char, array|
       if array.size == @board.centre_letter_offset
         array << char
       else
         array << "#{char}?"
       end
     end
+    .sort
     .join
-  end
-
-  def is_word_valid?(dictionary_word)
-    WordValidator.new(@board, dictionary_word).call
   end
 end
