98abe6bd4f018f3732230f50dfef771557b15104
Sam Russell <sam.russell@powershop.co.nz>
2015-11-09 15:42:33 +1300

    refactored board presenter

--- app/commands/create_board.rb
+++ app/commands/create_board.rb
@@ -1,11 +1,12 @@
 class CreateBoard
   NUMBER_OF_RANDOM_SEEDS = 10000
+  NUMBER_OF_LETTERS_IN_WORD = 9
 
   def call
     dictionary_word = DictionaryWord.seed_word
 
     Board.create!(dictionary_word: dictionary_word,
                   word_shuffle_seed: rand(NUMBER_OF_RANDOM_SEEDS),
-                  centre_letter_offset: rand(9))
+                  centre_letter_offset: rand(NUMBER_OF_LETTERS_IN_WORD))
   end
 end
--- app/presenters/board_presenter.rb
+++ app/presenters/board_presenter.rb
@@ -5,20 +5,6 @@ class BoardPresenter
     @board = board
   end
 
-  def grid
-    all_letters = @board.dictionary_word.word.chars
-
-    centre_cell = Cell.new(all_letters.delete_at(@board.centre_letter_offset), true)
-
-    other_cells = all_letters.map { |x| Cell.new(x, false) }
-
-    shuffled_other_cells = other_cells.shuffle(random: Random.new(@board.word_shuffle_seed))
-
-    shuffled_cells = shuffled_other_cells.insert(shuffled_other_cells.length/2, centre_cell)
-
-    grid_letters = shuffled_cells.each_slice(3).to_a
-  end
-
   def words
     @board.submitted_words.map { |submitted_word| submitted_word.dictionary_word.word }
   end
@@ -33,5 +19,37 @@ class BoardPresenter
 
   def id
     @board.id
+  end
+
+  def grid
+    grid_letters = shuffled_cells.each_slice(3).to_a
+  end
+
+  private
+
+  def shuffled_cells
+    shuffled_cells = shuffled_other_cells.insert(shuffled_other_cells.length/2, centre_cell)
+  end
+
+  def shuffled_other_cells
+    other_cells = other_letters.map { |letter| Cell.new(letter, false) }
+
+    other_cells.shuffle(random: Random.new(@board.word_shuffle_seed))
+  end
+
+  def other_letters
+    all_letters = @board.dictionary_word.word.chars
+
+    all_letters.delete_at(@board.centre_letter_offset)
+
+    all_letters
+  end
+
+  def centre_cell
+    Cell.new(centre_letter, true)
+  end
+
+  def centre_letter
+    @board.dictionary_word.word.chars[@board.centre_letter_offset]
   end
 end
